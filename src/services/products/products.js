58f9d9d13535b829bda41d011b9450ea16c6e058
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-07-09 16:15:27 +0200

    Adds image upload for products

--- src/services/products/products.js
+++ src/services/products/products.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { validateObjectId } from "../sharedMiddlewares.js"
 import * as Controllers from "../../controllers/products.js"
+import { prodImgParser } from "../../settings/cloudinary.js"
 
 const router = express.Router()
 
@@ -9,6 +10,6 @@ router.get("/:prodId", validateObjectId, Controllers.getSingleProduct)
 router.post("/", Controllers.addNewProduct)
 router.put("/:prodId", validateObjectId, Controllers.editProduct)
 router.delete("/:prodId", validateObjectId, Controllers.deleteProduct)
-router.post("/:prodId/uploadImage", validateObjectId, Controllers.uploadProductImage)
+router.post("/:prodId/uploadImage", validateObjectId, prodImgParser.single("prodImg"), Controllers.uploadProductImage)
 
 export default router
