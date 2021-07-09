32b49b7d9f49fe3de92c5c08345c8b7078993257
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-09 13:54:07 +0100

    status

--- src/services/cart/cart.js
+++ src/services/cart/cart.js
@@ -9,6 +9,9 @@ router.get("/", Controller.getCart)
 // Get Cart History
 router.get("/history", Controller.getCartHistory)
 
+// Update Cart Status
+router.put("/status", Controller.updateCartStatus)
+
 // Update Cart product
 router.put("/:prodId", Controller.updateCartProducts)
 
