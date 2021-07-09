fbd11b954a58e1ffaeef5343e83351bb6db81196
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-07-09 16:45:33 +0200

    Fixed reviews controllers

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -1,71 +1,61 @@
 import createError from "http-errors"
-import ReviewModel from "../models/reviews.js" 
+import ReviewModel from "../models/reviews.js"
 import ProductModel from "../models/products.js"
-import { getSingleProduct } from "../controllers/products.js"
 
-export const getSingleProductReviews = (req, res, next) => {
-    try {
-        const product = getSingleProduct()
-        const reviews = product.reviews
-        res.send(reviews)
-        
-    } catch (error) {
-        next(createError(500, 'Generic server error'))
-    }
+export const getSingleProductReviews = async (req, res, next) => {
+  try {
+    const reviews = await ProductModel.findById(req.params.prodId, { reviews: 1, _id: 0 }).populate("reviews")
+    res.send(reviews)
+  } catch (error) {
+    next(createError(500, "Generic server error"))
+  }
 }
 
 export const postProductReview = async (req, res, next) => {
-    try {
-        const product = getSingleProduct()
-        console.log(product);
-        const newReview = new ReviewModel(req.body)
-        const {_id} = await newReview.save()
-        await ProductModel.findByIdAndUpdate(req.params.prodId, {
-            $push:{
-                reviews:_id
-            }
-        })
-        res.status(201).send({_id})
-        
-    } catch (error) {
-        next(createError(500,'Generic server error' ))
-    }
+  try {
+    const newReview = new ReviewModel(req.body)
+    const { _id } = await newReview.save()
+    await ProductModel.findByIdAndUpdate(req.params.prodId, {
+      $push: {
+        reviews: _id,
+      },
+    })
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(createError(500, "Generic server error"))
+  }
 }
 
 export const editReview = async (req, res, next) => {
-    try {
-        const reviewId = req.params.reviewId
-        const editedReview = await ReviewModel.findByIdAndUpdate(reviewId,
-            req.body,
-            {
-                new:true,
-                runValidators:true
-            }
-            )
+  try {
+    const reviewId = req.params.reviewId
+    const editedReview = await ReviewModel.findByIdAndUpdate(reviewId, req.body, {
+      new: true,
+      runValidators: true,
+    })
 
-            if(editedReview){
-                res.send(editedReview)
-            }
-            else{
-                next(createError(404, `Review with id: ${reviewId} not found`))
-            }
-    } 
-    catch (error) {
-        next(createError(500,'Generic server error'))
+    if (editedReview) {
+      res.send(editedReview)
+    } else {
+      next(createError(404, `Review with id: ${reviewId} not found`))
     }
+  } catch (error) {
+    next(createError(500, "Generic server error"))
+  }
 }
 
 export const deleteReview = async (req, res, next) => {
-    try {
-        const reviewId = req.params.reviewId
-        const review = await ReviewModel.findByIdAndDelete(reviewId)
-        if(review){
-            res.status(204).send()
-        } 
-        else{
-            next(createError(404, `Review with id: ${reviewId} not found`))
-        }       
-    } catch (error) {
-        next(createError(500, 'Generic server error'))
+  try {
+    const reviewId = req.params.reviewId
+    await ProductModel.findOneAndUpdate({ reviews: reviewId }, { $pull: { reviews: reviewId } })
+
+    const review = await ReviewModel.findByIdAndDelete(reviewId)
+    if (review) {
+      res.json(review)
+    } else {
+      next(createError(404, `Review with id: ${reviewId} not found`))
     }
+  } catch (error) {
+    next(createError(500, "Generic server error"))
+  }
 }
